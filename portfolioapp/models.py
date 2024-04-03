7f2ad214b59ef39f41e5d21a8b408d9890115f53
emir-muradosilov <emir.muradosilov@yandex.ru>
2024-04-03 18:46:15 +0300

    Add migrations

--- portfolioapp/models.py
+++ portfolioapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    tittle = models.CharField(max_length = 100)
+    discriptions = models.CharField(max_length =250)
+    image = models.ImageField(upload_to ='portfolioapp/images/')
+    url = models.URLField(blank = True)
